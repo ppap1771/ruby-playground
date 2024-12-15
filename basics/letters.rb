a18d0269a81cee201a1803c72e4d97ec7317cd8b
ppap1771 <aayusggupta09022003@gmail.com>
2024-12-15 21:13:15 +0530

    add: basics-letters in ruby

---
+++ basics/letters.rb
@@ -0,0 +1,15 @@
+puts 'Hello.'
+puts 'Ruby rocks.'
+puts '5 is my favorite number... what is yours?'
+puts 'Snoopy says #%^?&*@! when he stubs his toe.'
+puts '     ' + "*"
+puts ''
+
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+
+# this will give o/p -> up/down 
+# Reason: as the character after the `/` is d which is not a recognized as a valid escape sequence
+puts 'up\down'
