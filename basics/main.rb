871388bf0e96884b201c46ddb093687774be378c
ppap1771 <aayusggupta09022003@gmail.com>
2024-12-15 20:50:57 +0530

    add: basic codes

---
+++ basics/main.rb
@@ -0,0 +1,18 @@
+# diffrence between puts and print 1. puts prints on new-line
+
+# puts "hello world"
+
+name = gets.chomp
+# puts "Hello, #{name}!"
+
+# def foo(name)
+#     res = "Night " + name + "!\n"
+#     return res
+# end
+
+# print foo(name)
+
+# This gives error due to data-types
+# puts 'Betty' + 12
+# puts 'Fred' * 'John'
+
