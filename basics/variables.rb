3fcf894c35069b69a644f8536eb940db5bc0a1b2
ppap1771 <aayusggupta09022003@gmail.com>
2024-12-16 16:06:59 +0530

    add: basic variables and assignments

---
+++ basics/variables.rb
@@ -0,0 +1,33 @@
+# ruby is a dynamically typed language and hence we dont really need to assign data-type on init
+# var = 8
+# puts var
+
+
+# puts "Enter your full name"
+
+# f = gets
+# m = gets
+# l = gets
+
+
+# puts f + m + l 
+
+# """
+#   Output: 
+#   Enter your full name
+#   ayush
+#   balram
+#   gupta
+#   ayush
+#   balram
+#   gupta
+# """
+
+puts "Enter your full name"
+
+f = gets.chomp
+m = gets.chomp
+l = gets.chomp
+
+
+puts f + " " + m + " "  + l 
