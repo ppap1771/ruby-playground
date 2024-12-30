7d539b72848bb7e5cab40457630f1ef52dcefc73
ppap1771 <aayusggupta09022003@gmail.com>
2024-12-30 23:08:02 +0530

    add: CRUDoperations

--- dummy/config/routes.rb
+++ dummy/config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "products#index"
+
+  # get "/products", to: "products#index"
+  # post "/products", to: "products#create"
+  # get "/products/:id", to: "products#show"
+  resources :products
 end
